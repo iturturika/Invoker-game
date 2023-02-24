e115f1e582e9f3e0f6ffc29718d74397a2d575a9
iturturika <iturturika89@gmail.com>
2023-02-24 19:29:52 +0200

    func random spell added

--- src/actions/randomSpell.js
+++ src/actions/randomSpell.js
@@ -1,15 +1,5 @@
-import cold_snap from '../img/invoker_cold_snap.png';
-import ghost_walk from '../img/invoker_ghost_walk.png';
-import ice_wall from '../img/invoker_ice_wall.png';
-import emp from '../img/invoker_emp.png';
-import tornado from '../img/invoker_tornado.png';
-import alacrity from '../img/invoker_alacrity.png';
-import sun_strike from '../img/invoker_sun_strike.png';
-import forge_spirit from '../img/invoker_forge_spirit.png';
-import chaos_meteor from '../img/invoker_chaos_meteor.png';
-import deafening_blast from '../img/invoker_deafening_blast.png';
 
-const arr  = [cold_snap, ghost_walk, ice_wall, emp, tornado, alacrity, sun_strike, forge_spirit, chaos_meteor, deafening_blast];
+const arr  = ["cold snap", "ghost walk", "ice wall", "emp", "tornado", "alacrity", "sun strike", "forge spirit", "chaos meteor", "deafening blast"];
 export const randomSpell = (setRandomSpell) => {
     const randomIndex = Math.floor(Math.random() * arr.length);
     setRandomSpell(arr[randomIndex]);
--- src/pages/StartedGame/StartedGame.js
+++ src/pages/StartedGame/StartedGame.js
@@ -7,13 +7,37 @@ import exort from '../../img/invoker_exort.png';
 import invoke from '../../img/invoker_invoke.png';
 import nospell from '../../img/no_spell.png';
 
+import cold_snap from '../../img/invoker_cold_snap.png';
+import ghost_walk from '../../img/invoker_ghost_walk.png';
+import ice_wall from '../../img/invoker_ice_wall.png';
+import emp from '../../img/invoker_emp.png';
+import tornado from '../../img/invoker_tornado.png';
+import alacrity from '../../img/invoker_alacrity.png';
+import sun_strike from '../../img/invoker_sun_strike.png';
+import forge_spirit from '../../img/invoker_forge_spirit.png';
+import chaos_meteor from '../../img/invoker_chaos_meteor.png';
+import deafening_blast from '../../img/invoker_deafening_blast.png';
+
+
 import { restartGame } from '../../actions/restartGame';
 const StartedGame = (props) => {
+
   return (
      <div className='mainSection'>
         <h1>Invoker Game by undefined</h1>
         <img src={invoker} alt='invoker' className='img'></img>
-        <div className='curent__spell'><img src={props.randomSpell} alt="randomSpell"></img><p>Forge Spirit</p></div>
+        <div className='curent__spell'>
+          {props.randomSpell === 'cold snap' ? <><img src={cold_snap} alt="randomSpell"></img><p>{props.randomSpell}</p></> : null}
+          {props.randomSpell === 'ghost walk' ? <><img src={ghost_walk} alt="randomSpell"></img><p>{props.randomSpell}</p></> : null}
+          {props.randomSpell === 'ice wall' ? <><img src={ice_wall} alt="randomSpell"></img><p>{props.randomSpell}</p></> : null}
+          {props.randomSpell === 'emp' ? <><img src={emp} alt="randomSpell"></img><p>{props.randomSpell}</p></> : null}
+          {props.randomSpell === 'tornado' ? <><img src={tornado} alt="randomSpell"></img><p>{props.randomSpell}</p></> : null}
+          {props.randomSpell === 'alacrity' ? <><img src={alacrity} alt="randomSpell"></img><p>{props.randomSpell}</p></> : null}
+          {props.randomSpell === 'sun strike' ? <><img src={sun_strike} alt="randomSpell"></img><p>{props.randomSpell}</p></> : null}
+          {props.randomSpell === 'forge spirit' ? <><img src={forge_spirit} alt="randomSpell"></img><p>{props.randomSpell}</p></> : null}
+          {props.randomSpell === 'chaos meteor' ? <><img src={chaos_meteor} alt="randomSpell"></img><p>{props.randomSpell}</p></> : null}
+          {props.randomSpell === 'deafening blast' ? <><img src={deafening_blast} alt="randomSpell"></img><p>{props.randomSpell}</p></> : null}
+        </div>
         <button className='restart' onClick={() => {restartGame(props.setGameState)}}>Restart</button>
         <ul className='window'>
           <li className='circle'>
