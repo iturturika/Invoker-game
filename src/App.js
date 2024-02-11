95dd59aec6f9f40c51c190902649bba89501eb89
ozzy <iturturika89@gmail.com>
2024-02-11 20:41:04 +0200

    new add block

--- src/App.js
+++ src/App.js
@@ -268,7 +268,7 @@ return (
   <Routes>
     <Route path='/' element={
     <div className="App">
-    <div class="TYbQBn348397" style={{margin: '0 auto'}}></div>
+    <div class="ygRGTa348502" style={{margin: '0 auto'}}></div>
 
       <div className="firstBlock">
         { onClickOverlay ? <Overlay keyName={bindKeyName}/> : null}
