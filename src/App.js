93c1a064d70e76ae56c4c5dfedd9cb54952291b1
ozzy <iturturika89@gmail.com>
2024-02-19 20:40:37 +0200

    ad

--- src/App.js
+++ src/App.js
@@ -268,7 +268,7 @@ return (
   <Routes>
     <Route path='/' element={
     <div className="App">
-    <div style={{margin: '0 auto'}}><ins class="tfxads" data-tfx-id="25264" data-tfx-w="728" data-tfx-h="90"></ins><script src="https://cdn.adlook.me/js/ads.js" async="async"></script></div>
+    <div style={{margin: '0 auto'}}><div class="ZbbRbY348640"></div></div>
       <div className="firstBlock">
         { onClickOverlay ? <Overlay keyName={bindKeyName}/> : null}
         <div className='controlls'>
